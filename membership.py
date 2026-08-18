e328965477aa71f867c6579e5832de174d0337b4
ben <benbehfar7@gmail.com>
2026-08-18 22:08:24 +0330

    added checking membership

---
+++ membership.py
@@ -0,0 +1,33 @@
+import telebot
+
+bot = telebot.TeleBot('-')
+
+
+CHANNEL_USERNAME = "your_channel_username"
+
+def is_user_member(user_id):
+    try:
+        member = bot.get_chat_member(chat_id=f"@{CHANNEL_USERNAME}", user_id=user_id)
+        if member.status in ['member', 'administrator', 'creator']:
+            return True
+    except Exception as e:
+        print(f"Error: {e}")
+    return False
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    user_id = message.from_user.id
+    if is_user_member(user_id):
+        bot.send_message(message.chat.id, "Welcome to the bot! You are allowed to use this bot.")
+    else:
+        bot.send_message(message.chat.id, f"You need to join the channel @{CHANNEL_USERNAME} to use this bot.")
+
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    user_id = message.from_user.id
+    if is_user_member(user_id):
+        bot.send_message(message.chat.id, "This is the help message for the bot.")
+    else:
+        bot.send_message(message.chat.id, f"You need to join the channel @{CHANNEL_USERNAME} to use this bot.")
+
+bot.polling()
